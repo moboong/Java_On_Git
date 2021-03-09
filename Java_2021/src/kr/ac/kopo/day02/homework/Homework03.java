608e2f6538e46696ff36f7a87de07afc2f010c36
황준호 <@DESKTOP-OG1E333>
2021-03-09 22:55:37 +0900

    안녕

--- Java_2021/src/kr/ac/kopo/day02/homework/Homework03.java
+++ Java_2021/src/kr/ac/kopo/day02/homework/Homework03.java
@@ -11,21 +11,26 @@ public class Homework03 {
 		System.out.print("지불액수 입력: ");
 		int payment = sc.nextInt();
 		sc.close();
-		
+
 		int change = payment - price;
-		
+		// change = (a * 1000) + (b * 500) + (c * 100) + (d * 50) + (e * 10)
+		int a = change / 1000;
+		int b = (change - (a * 1000)) / 500;
+		int c = (change - (a * 1000) - (b * 500)) / 100;
+		int d = (change - (a * 1000) - (b * 500) - (c * 100)) / 50;
+		int e = (change - (a * 1000) - (b * 500) - (c * 100) - (d * 50)) / 10;
+
 		if (price > payment) {
 			System.out.println((price - payment) + "원이 부족합니다.");
 		} else if (price == payment) {
 			System.out.println("거스름돈이 없습니다.");
 		} else {
-			System.out.println(change);
-			System.out.println("1000원: "+ change % 1000);
-			System.out.println("500원: "+ (change % 1000) * 1000);
-//			System.out.println("100원: "+ change % );
-//			System.out.println("50원: "+ change % );
-//			System.out.println("10원: "+ change % 2);
-			
+			System.out.println("거스름돈: " + change);
+			System.out.println("1000원: " + a);
+			System.out.println("500원: " + b);
+			System.out.println("100원: " + c);
+			System.out.println("50원: " + d);
+			System.out.println("10원: " + e);
 		}
 
 	}
