9d67956e8e715e54db6852587a9efe16c39a2f89
황준호 <luffy0318@gmail.com>
2021-03-10 23:02:05 +0900

    숙제 완료

--- Java_2021/src/kr/ac/kopo/day03/homework/HomeworkMain08.java
+++ Java_2021/src/kr/ac/kopo/day03/homework/HomeworkMain08.java
@@ -4,24 +4,29 @@ public class HomeworkMain08 {
 
 	public static void main(String[] args) {
 		int var = 0;
-		for(int i = 0; i < 9; i++) {
-			if(i > 4) {
-				var += 2; 
+		for (int i = 0; i < 9; i++) {
+			if (i > 4) {
+				var += 2;
 			}
-			for(int j = 0; j <= i-var; j++) { //0~4
-				System.out.print("*");
-			}
-			for(int j = i+1-var; j < 9-(i+1-var); j++) { // 7, 5, 3, 1, 0
-				System.out.print(" ");
-			}
-			for(int j = 9-(i+1-var); j < 9; j++) {
-				System.out.print("*");
+			if (i == 4) {
+				for (int j = 0; j < 9; j++) {
+					System.out.print("*");
+				}
+			} else {
+				for (int j = 0; j <= i - var; j++) {
+					System.out.print("*");
+				}
+				for (int j = i + 1 - var; j < 9 - (i + 1 - var); j++) {
+					System.out.print(" ");
+				}
+				for (int j = 9 - (i + 1 - var); j < 9; j++) {
+					System.out.print("*");
+				}
 			}
 			System.out.println();
-			
-			// 0~4까지는 var이 0이어서 구간들에 영향을 끼치지 않음.
-			// 5부터 var값이 0이 아닌 값이 되어 구간에 영향을 준다.
-			// 구간은 2씩 줄어듦으로 var += 2를 해준다.
+			// 0 ~ 3까지는 var이 0이어서 구간들에 영향을 끼치지 않음.
+			// 4는 홀수개 이므로 예외처리
+			// 5부터 var값이 0이 아닌 값이 되어 구간에 영향을 준다. -> 구간은 2씩 줄어듦으로 var += 2를 해준다.
 		}
 
 	}
