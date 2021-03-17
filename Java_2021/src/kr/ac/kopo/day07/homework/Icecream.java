e4c29294f77893f2d091f7ed99e6679b1d730431
DESKTOP-KV7BVEF\HP <luffy0318@gmail.com>
2021-03-17 17:24:21 +0900

    0317 끝

--- Java_2021/src/kr/ac/kopo/day07/homework/Icecream.java
+++ Java_2021/src/kr/ac/kopo/day07/homework/Icecream.java
@@ -1,78 +1,39 @@
 package kr.ac.kopo.day07.homework;
 
-import java.util.Scanner;
-
 public class Icecream {
 	String name;
 	int price;
-	Scanner sc = new Scanner(System.in);
-	
-	int getNum() {
-		System.out.print("아이스크림 몇개 구입하실? : ");
-		int num = sc.nextInt();
-		sc.nextLine();
-		return num;
-	}
-	
-	
-	void setPrice(String msg) {
-		System.out.print(msg);
-		int input = sc.nextInt();
-		sc.nextLine();
-		this.price = input;
 
+	Icecream() {
+		this("Unkown", -1);
 	}
 
-	void setName(String msg) {
-		System.out.print(msg);
-		String input = sc.nextLine();
-		this.name = input;
-	}
-	
-	/**
-	 * 배열 전체를 돌며 초기화 작업.
-	 * @param ic 아이스크림 객체의 배열
-	 */
-	void setMember(Icecream[] ic) {
-		for(int i = 0; i < ic.length; i++) {
-			System.out.println("*** " + (i + 1) + "번째 아이스크림 구매정보 입력 ***");
-			ic[i] = new Icecream();
-			ic[i].setName("아이스크림 이름 : ");
-			ic[i].setPrice("아이스크림 가격 : ");
-		}
-	}
-	
-	/**
-	 * 객체의 멤버변수 name, price 출력
-	 */
-	void print() {
-		System.out.println(this.name + "       " + this.price);
-
-	}
-	
-	/**
-	 * 배열 전체를 돌며 출력
-	 * @param ic 아이스크림 객체의 배열
-	 */
-	void print(Icecream[] ic) {
-		System.out.println("< 총 " + ic.length + "개의 아이스크림 구매정보 출력 >");
-		System.out.println("번호   아이스크림이름   아이스크림가격");
-		for (int i = 0; i < ic.length; i++) {
-			System.out.print((i + 1) + "      ");
-			ic[i].print();
-		}
-	}
-	
-	/**
-	 * 배열 전체를 돌며 가격 변수만 합산
-	 * @param ic 아이스크림 객체의 배열
-	 */
-	void getSum(Icecream[] ic) {
-		int sum = 0;
-		for (int i = 0; i < ic.length; i++) {
-			sum += ic[i].price;
-		}
-		System.out.println("총계 : " + sum);
+	Icecream(String name) {
+		this(name, -1);
 	}
 
+	Icecream(int price) {
+		this("Unkown", price);
+	}
+
+	Icecream(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	String getName() {
+		return name;
+	}
+
+	int getPrice() {
+		return price;
+	}
+
+	void setName(String name) {
+		this.name = name;
+	}
+
+	void setPrice(int price) {
+		this.price = price;
+	}
 }
--- Java_2021/src/kr/ac/kopo/day07/homework/IcecreamMain.java
+++ Java_2021/src/kr/ac/kopo/day07/homework/IcecreamMain.java
@@ -3,14 +3,9 @@ package kr.ac.kopo.day07.homework;
 public class IcecreamMain {
 
 	public static void main(String[] args) {
-		Icecream ic = new Icecream(); // 이놈은 그럼?
-		int num = ic.getNum();
-		Icecream[] arrIc = new Icecream[num];
-		
-		ic.setMember(arrIc);
-		ic.print(arrIc);
-		ic.getSum(arrIc);
-		
+		IcecreamMarket m = new IcecreamMarket();
+		m.info();
+
 	}
 
 }
