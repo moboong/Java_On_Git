7a0fce032dbdf32f98df6738ee8dde840c658b14
DESKTOP-KV7BVEF\HP <luffy0318@gmail.com>
2021-03-24 15:35:29 +0900

    0324 수업 끝

---
+++ Java_2021/src/kr/ac/kopo/day13/api/DateMain.java
@@ -0,0 +1,47 @@
+package kr.ac.kopo.day13.api;
+
+import java.util.Date;
+
+
+
+public class DateMain {
+
+	public static void main(String[] args) {
+		
+		Dog dog = new Dog("멍이", 2);
+		System.out.println(dog); //오버라이드 안하면 당연하게도 주소값이 출력 되겠지?
+
+
+		// 오늘은 3월 24일입니다.
+		Date d = new Date();
+		System.out.println("d :" + d); //?엥 주소값 찍혀야하는데? 
+		// 이거 왜 그러냐 -> toString오버라이딩해서
+		
+		int year = d.getYear() + 1900;
+		int month = d.getMonth() + 1;
+		int date = d.getDate();
+		
+		System.out.println("오늘은 " + year + "년 " + month + "월 " + date + "일");
+
+	}
+
+}
+
+class Dog{
+	String name;
+	int age;
+	
+	public Dog() {
+		
+	}
+	public Dog(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Dog [name=" + name + ", age=" + age + "]";
+	}
+	
+	
+}
---
+++ Java_2021/src/kr/ac/kopo/day13/api/DateMain01.java
@@ -0,0 +1,39 @@
+package kr.ac.kopo.day13.api;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateMain01 {
+
+	public static void main(String[] args) {
+		Calendar c = Calendar.getInstance(); // 실제 타입은 그레고리
+		System.out.println("c: " + c);
+
+		int year = c.get(Calendar.YEAR);
+		int month = c.get(Calendar.MONTH) + 1;
+		int day = c.get(Calendar.DAY_OF_WEEK);
+
+		String[] dayArr = { "", "일", "월", "화", "수", "목", "금", "토" };
+		System.out.println(year);
+		System.out.println(month);
+		System.out.println(dayArr[day]);
+
+		// 2021 5월5일 무슨 요일?
+		c.set(2021, 5 - 1, 5); // 5-1해야 5월이 구해져.
+		day = c.get(Calendar.DAY_OF_WEEK);
+		System.out.println(dayArr[day]);
+
+		// 5월의 마지막 날이 며칠?
+		int lastday = c.getActualMaximum(Calendar.DAY_OF_MONTH);
+		System.out.println(lastday);
+
+		String pattern = "yyyy-MM-dd HH:mm:ss E";
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		System.out.println(sdf.format(new Date()));//여기에 c를 넣고 싶어. 그러려면 c를 Date로 변경
+		
+		Date d = c.getTime();
+		System.out.println(sdf.format(d));
+	}
+
+}
